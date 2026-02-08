770dc892af70e3ae472cffd975e075c091fa50f1
Lifei Liu <federerl6666@gmail.com>
2026-02-08 11:26:32 +0800

    enable message intents and ping reply

--- src/index.js
+++ src/index.js
@@ -2,12 +2,25 @@ import { Client, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === "ping") {
+    message.reply("pong 🏓");
+  }
+});
+
 console.log("Token loaded:", Boolean(process.env.DISCORD_TOKEN));
 console.log("Token length:", process.env.DISCORD_TOKEN?.length);
 
